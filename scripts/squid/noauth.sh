d8d264b49019682ec6dca5c6626d78077fabaaf7
Tomer Shaiman <tshaiman@microsoft.com>
2023-06-03 19:06:05 +0300

    squid version 5

--- scripts/squid/noauth.sh
+++ scripts/squid/noauth.sh
@@ -1,7 +1,14 @@
- 
+#!/bin/bash
 #######################################
 echo "Deploy Squid Proxy Server"
 #########################################
-sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install squid -y
-cat ./squid-noauth.conf | sudo tee /etc/squid/squid.conf
-sudo systemctl restart squid.service
+sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install -y libssl-dev pkg-config build-essential autoconf apache2-utils
+
+wget  http://www.squid-cache.org/Versions/v5/squid-5.9.tar.gz
+tar xvzf squid-5.9.tar.gz
+cd squid-5.9
+./configure --with-openssl  --enable-ssl-crtd
+sudo make && sudo make install
+sudo rm /usr/local/squid/etc/squid.conf && sudo cp ./squid-noauth.conf /usr/local/squid/etc/squid.conf
+sudo chmod 777 /usr/local/squid/var/logs
+sudo /usr/local/squid/sbin/squid start
