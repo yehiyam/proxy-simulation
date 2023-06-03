16852d43c81005d23b82da222b9905c661817bfd
Tomer Shaiman <tshaiman@microsoft.com>
2023-06-03 08:41:36 +0300

    proxy config

--- scripts/cluster/main.sh
+++ scripts/cluster/main.sh
@@ -2,24 +2,24 @@
 
 export SQUID_NOAUTH_IP=$1
 
-#Deploy K8s Single Node KubeAdm cluster
+#######################################
+echo "Deploy K8s Single Node KubeAdm cluster"
+#########################################
 sudo -i
 bash <(curl -s https://raw.githubusercontent.com/killer-sh/cks-course-environment/master/cluster-setup/latest/install_master.sh)
 
-
 echo "Remove Taints on master node"
 kubectl taint nodes --all node-role.kubernetes.io/control-plane-
-
 
 echo "Patch Kube config"
 mkdir -p /home/azureuser/.kube
 sudo cp -i /etc/kubernetes/admin.conf /home/azureuser/.kube/config
 sudo chown azureuser:azureuser /home/azureuser/.kube/config
 
-
+#################################################
 echo "Install Utils"
+################################################
 # Install Azure CLI
-
 sudo apt-get update
 sudo apt-get install -y ca-certificates curl apt-transport-https lsb-release gnupg
 
@@ -46,9 +46,11 @@ curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scr
 chmod 700 get_helm.sh
 ./get_helm.sh
 
-# apply simulation.yaml
+###############################
+echo "apply Network Policy"
+###############################
 echo "Donwload simulation.yaml"
 wget https://raw.githubusercontent.com/shashankbarsin/proxy-simulation/main/manifests/simulation.yaml
 perl -i -pe "s/<SQUID_NOAUTH_IP>/$(echo -n $SQUID_NOAUTH_IP)/g" simulation.yaml
 cp simulation.yaml /home/azureuser/simulation.yaml
-#kubectl apply -f /home/azureuser/simulation.yaml
+kubectl apply -f simulation.yaml
--- scripts/squid/noauth.sh
+++ scripts/squid/noauth.sh
@@ -1,20 +1,7 @@
-#!/bin/bash
-
-# sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install -y libssl-dev pkg-config build-essential autoconf apache2-utils
-
-# wget http://www.squid-cache.org/Versions/v4/squid-4.12.tar.gz 
-# tar xvzf squid-4.12.tar.gz
-# cd squid-4.12
-# ./configure --with-openssl  --enable-ssl-crtd
-# sudo make && sudo make install
-# sudo rm /usr/local/squid/etc/squid.conf && sudo cp ../squid-noauth.conf /usr/local/squid/etc/squid.conf
-# sudo chmod 777 /usr/local/squid/var/logs
-# sudo /usr/local/squid/sbin/squid start
-
-
-echo "new Squid installation Process -Tshaiman"
-
-
+ 
+#######################################
+echo "Deploy Squid Proxy Server"
+#########################################
 sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install squid
-sudo cp ../squid-noauth.conf /etc/squid/squid.conf
+sudo cp ./squid-noauth.conf /etc/squid/squid.conf
 sudo systemctl restart squid.service
